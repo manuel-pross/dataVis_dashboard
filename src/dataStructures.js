c886357fb2667a100cfab436b69ced80d20cadac
fhs49068 <mpross.mmt-m2022@fh-salzburg.ac.at>
2023-01-04 11:54:58 +0100

    feat: add effectivness to typecombos

--- src/dataStructures.js
+++ src/dataStructures.js
@@ -169,7 +169,7 @@ export const resitencesMap = new Map([
         TypesEnum.Fairy,
       ],
       completeResistences: [],
-      weaknesses: [],
+      weaknesses: [TypesEnum.Ground, TypesEnum.Rock, TypesEnum.Water],
     },
   ],
   [
@@ -183,7 +183,12 @@ export const resitencesMap = new Map([
   [
     TypesEnum.Water,
     {
-      simpleResistences: [TypesEnum.Steel, TypesEnum.Fire, TypesEnum.Water],
+      simpleResistences: [
+        TypesEnum.Steel,
+        TypesEnum.Fire,
+        TypesEnum.Water,
+        TypesEnum.Ice,
+      ],
       completeResistences: [],
       weaknesses: [TypesEnum.Grass, TypesEnum.Electric],
     },
@@ -365,9 +370,11 @@ function generateCombinations() {
   typesColorAmount.forEach((type) => {
     typeCombinations.push({
       name: type.name,
+      amountRes: 0,
       amountDoubleRes: 0,
-      amountQadrupleRes: 0,
       amountCompleteRes: 0,
+      amountWeak: 0,
+      amountDoubleWeak: 0,
       amountPokemon: 0,
     });
     typesColorAmount.forEach((type2) => {
@@ -383,16 +390,152 @@ function generateCombinations() {
         if (!foundReverseCombination)
           typeCombinations.push({
             name: newCombination,
+            amountRes: 0,
             amountDoubleRes: 0,
-            amountQadrupleRes: 0,
             amountCompleteRes: 0,
+            amountWeak: 0,
+            amountDoubleWeak: 0,
             amountPokemon: 0,
           });
       }
     });
   });
 }
+//111 drache elektro passt nicht
+// function calcTypeCombiWeakAndEffect() {
+//   typeCombinations.forEach((combi) => {
+//     const types = combi.name.split("/");
+//     const type1 = types[0];
+//     const type2 = types[1];
 
-function calcTypeCombiWeakAndEffect() {}
+//     const type1SimpleRes = resitencesMap.get(type1).simpleResistences;
+//     const type1Weaknesses = resitencesMap.get(type1).weaknesses;
+//     const type1CompleteRes = resitencesMap.get(type1)?.completeResistences;
+//     const type2SimpleRes = resitencesMap.get(type2)?.simpleResistences;
+//     const type2Weaknesses = resitencesMap.get(type2)?.weaknesses;
+//     const type2CompleteRes = resitencesMap.get(type2)?.completeResistences;
+
+//     type1SimpleRes.forEach((type1Resistence) => {
+//       combi.amountRes++;
+//       if (type2) {
+//         type2Weaknesses.forEach((type2Weakness) => {
+//           if (type2Weakness === type1Resistence) combi.amountRes--;
+//         });
+
+//         type2SimpleRes.forEach((type2Resistence) => {
+//           if (type1Resistence === type2Resistence) {
+//           }
+//         });
+//       }
+//     });
+
+//     type1Weaknesses.forEach((type1Weakness) => {
+//       combi.amountWeak++;
+
+//       if (type2) {
+//         type2SimpleRes.forEach((type2Resistence) => {
+//           if (type1Weakness === type2Resistence) combi.amountWeak--;
+//         });
+//         type2Weaknesses.forEach((type2Weakness) => {
+//           if (type2Weakness === type1Weakness) {
+//             combi.amountDoubleWeak++;
+//             combi.amountRes += -2;
+//           }
+//         });
+//       }
+//     });
+
+//     if (type2) {
+//       type2SimpleRes.forEach((type2Resistence) => {
+//         combi.amountRes++;
+//         type1Weaknesses.forEach((type1Weakness) => {
+//           if (type1Weakness === type2Resistence) combi.amountRes--;
+//         });
+//       });
+
+//       type2Weaknesses.forEach((type2Weakness) => {
+//         combi.amountWeak++;
+
+//         type1SimpleRes.forEach((type1Resistence) => {
+//           if (type2Weakness === type1Resistence) combi.amountWeak--;
+//         });
+//       });
+
+//       combi.amountCompleteRes +=
+//         resitencesMap.get(type2).completeResistences.length;
+//     }
+
+//     combi.amountCompleteRes +=
+//       resitencesMap.get(type1).completeResistences.length;
+//   });
+
+//   console.log(typeCombinations);
+// }
+
+function setWeakAndEffect() {
+  typeCombinations.forEach((typeCombi) => {
+    for (const [key, value] of Object.entries(TypesEnum)) {
+      const multiplier = getMultiplier(value, typeCombi.name);
+      translateMultiplier(multiplier, typeCombi);
+    }
+  });
+}
+
+function translateMultiplier(multiplier, typeCombi) {
+  switch (multiplier) {
+    case 0:
+      typeCombi.amountCompleteRes += 1;
+      break;
+    case 0.25:
+      typeCombi.amountDoubleRes += 1;
+      break;
+    case 0.5:
+      typeCombi.amountRes += 1;
+      break;
+    case 2:
+      typeCombi.amountWeak += 1;
+      break;
+    case 4:
+      typeCombi.amountDoubleWeak += 1;
+      break;
+    default:
+      break;
+  }
+}
+
+function getMultiplier(attackingType, defendingTypeCombi) {
+  // keys is a list of letters found in the types of attacks/defenses
+  const keys = [..."BWSEIRNulkcDPotyeG"];
+
+  // getIndex is a single case statement.
+  // it checks each of keys, one-by-one, falling through until we've found the proper index
+  const getIndex = (x) => keys.findIndex((c) => x.match(c));
+
+  // encodedValues is a list, indexed by `keys`, where each value is 7-characters.
+  const encodedValues =
+    "kjwhcgnj2xd6elihtlneemw82duxijsazl3sh4iz5akjmlmsqds06xf1sbb8d0rl1nu7a2kjwi3mykjwlbpmk1up4mzl1iuenedor0bdmkjwmpk6rhcg4h3en3pew5";
+
+  // the 7-character value (e.g., B=0="kjwhcgn", W=1="j2xd6el") were created by
+  // turning base4 values into base36, so let's turn this back into a string the same way
+  const valuesForAttack = parseInt(
+    encodedValues.substr(getIndex(attackingType) * 7, 7),
+    36
+  ).toString(4);
+
+  // valuesForAttack is indexed by defenseType.  The value will be 0..3, depending on the multiplier
+
+  // let's get an array of the multipliers and reduce...
+  const multiplier = defendingTypeCombi
+    .split("/")
+    .reduce(
+      (oldMultiplier, defenseType) =>
+        oldMultiplier * [0, 0.5, 1, 2][valuesForAttack[getIndex(defenseType)]],
+      1
+    );
+  return multiplier;
+}
 
 generateCombinations();
+setWeakAndEffect();
+console.log(typeCombinations);
+console.log(getMultiplier("Bug", "Elektric/Psychic"));
